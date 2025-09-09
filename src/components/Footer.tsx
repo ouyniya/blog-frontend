665d2b8be025b68ab3de49430a5fda1bf2c32a71
niya-s <niya.somkerd@gmail.com>
2025-09-09 14:25:19 +0700

    feat: navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between w-full min-h-35 bg-sky-600 rounded-t-[80px] p-4 text-white">
+    <div className="flex justify-between w-full min-h-35 bg-sky-800 rounded-t-[80px] p-4 text-white">
       <div className="flex justify-start items-end pl-[2%]">
         <Snowflake size={"75px"} />
         <div className="flex h-full justify-start pt-[5%]">
-          <Snowflake size={"35px"} />
+          <Snowflake size={"35px"} className="text-sky-400" />
         </div>
       </div>
       <div className="flex items-center h-full text-center">
@@ -16,10 +16,16 @@ const Footer = () => {
           <strong>Nysdev</strong>
           <br />
           <div className="flex gap-4 justify-center font-semibold">
-            <Link to="/privacy">
+            <Link
+              to="/privacy"
+              className="text-sky-300   hover:text-sky-500 duration-300"
+            >
               <span>Privacy Policy</span>
             </Link>
-            <Link to="/cookie">
+            <Link
+              to="/cookie"
+              className="text-sky-300 hover:text-sky-500 duration-300"
+            >
               <span>Cookie Policy</span>
             </Link>
           </div>
@@ -27,7 +33,7 @@ const Footer = () => {
       </div>
       <div className="flex justify-start items-end pr-[2%]">
         <div className="flex h-full justify-start pt-[5%]">
-          <Snowflake size={"35px"} />
+          <Snowflake size={"35px"} className="text-sky-400" />
         </div>
         <Snowflake size={"75px"} />
       </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,36 @@
+import { PinIcon, Megaphone } from "lucide-react";
+import { Marquee } from "@/components/magicui/marquee";
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
-export default NavBar
+    <div className="flex flex-col gap-4 mb-[5%]">
+      <div className="w-full min-h-[400px] bg-sky-200 rounded-b-[100px]"></div>
+      <div className="flex justify-between items-center pl-[5%]">
+        {/* location */}
+        <div className="flex gap-2 basis-1/5">
+          <PinIcon />
+          <p>Bangkok</p>
+        </div>
+
+        {/* Marquee */}
+        <div className="flex gap-4 items-center basis-4/5 pr-[2%]">
+          <div>
+            <Megaphone className="text-sky-600" />
+          </div>
+          <Marquee
+            pauseOnHover
+            className="[--duration:35s] hover:cursor-default"
+          >
+            <p className="text-sky-600 text-sm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+              veritatis illum accusamus necessitatibus, voluptatum nisi
+              assumenda consectetur cum nobis pariatur neque ab aperiam quas?
+              Impedit aspernatur nulla eum corporis aperiam.
+            </p>
+          </Marquee>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default NavBar;
