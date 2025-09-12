fb6a238f296d21f64b2928cfc267e22cfef44739
niya-s <niya.somkerd@gmail.com>
2025-09-12 14:41:25 +0700

    fix: server allowance

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // เปิดให้เข้าผ่าน network หรือ domain mock ได้
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
